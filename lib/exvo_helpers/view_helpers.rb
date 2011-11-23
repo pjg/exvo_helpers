9cbf479f156d0c23272c94e7d54dbea416cbf0ce
Paweł Gościcki <pawel.goscicki@gmail.com>
2011-11-23 15:44:23 +0100

    Force http in production javascript bundles
    cdn.exvo.com does not have SSL certificate (is uses a generic one), so
    Firefox 8 on Windows (for example) refuses to load bundles via https.

--- lib/exvo_helpers/view_helpers.rb
+++ lib/exvo_helpers/view_helpers.rb
@@ -2,11 +2,12 @@ module Exvo
 
   module ViewHelpers
 
-    # special link starting with '//' in production so that the webserver can choose between HTTP and HTTPS
     def javascript_bundle_include_tag(bundle)
       case Exvo::Helpers.env.to_sym
       when :production
-        javascript_include_tag "//#{ Exvo::Helpers.cdn_host }/javascripts/#{bundle}.js"
+        # FIXME change back to '//' in production so that the webserver can choose between HTTP and HTTPS
+        # note, that it requires a proper SSL certificate installed for cdn.exvo.com domain
+        javascript_include_tag "http://#{ Exvo::Helpers.cdn_host }/javascripts/#{bundle}.js"
       when :staging
         javascript_include_tag "http://#{ Exvo::Helpers.cdn_host }/javascripts/#{bundle}.js"
       else
--- spec/exvo_helpers/view_helpers_spec.rb
+++ spec/exvo_helpers/view_helpers_spec.rb
@@ -12,7 +12,7 @@ describe Exvo::ViewHelpers do
 
     it "should return the javascript_include_tag based on env" do
       Exvo::Helpers.stub(:env).and_return('production')
-      view_helper.should_receive(:javascript_include_tag).with("//cdn.exvo.com/javascripts/plugins.js")
+      view_helper.should_receive(:javascript_include_tag).with("http://cdn.exvo.com/javascripts/plugins.js")
       view_helper.javascript_bundle_include_tag("plugins")
     end
   end
