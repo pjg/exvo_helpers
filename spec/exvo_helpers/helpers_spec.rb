cc7ab18154f192375dfe11b4c310a02b03962275
Paweł Gościcki <pawel.goscicki@gmail.com>
2012-06-13 13:08:31 +0200

    Add missing specs for direct setting
    Of the auth_client_id and auth_client_secret options.

--- spec/exvo_helpers/helpers_spec.rb
+++ spec/exvo_helpers/helpers_spec.rb
@@ -88,62 +88,70 @@ describe Exvo::Helpers do
   describe "ENV settings override the defaults" do
     # all *host* methods are defined the same using metaprogramming, only testing for 1 is enough
     let(:cdn_host) { "test.cdn.exvo.com" }
-    let(:sso_cookie_domain) { "exvo.dev" }
     let(:auth_client_id) { '123' }
     let(:auth_client_secret) { 'abc' }
     let(:auth_require_ssl) { 'false' }
+    let(:sso_cookie_domain) { "exvo.dev" }
 
     before do
       # clear any previous memoization
       Exvo::Helpers.cdn_host = nil
-      Exvo::Helpers.sso_cookie_domain = nil
       Exvo::Helpers.auth_client_id = nil
       Exvo::Helpers.auth_client_secret = nil
       Exvo::Helpers.auth_require_ssl = nil
+      Exvo::Helpers.sso_cookie_domain = nil
 
       # set ENV
       ENV["CDN_HOST"] = cdn_host
-      ENV["SSO_COOKIE_DOMAIN"] = sso_cookie_domain
       ENV["AUTH_CLIENT_ID"] = auth_client_id
       ENV["AUTH_CLIENT_SECRET"] = auth_client_secret
       ENV["AUTH_REQUIRE_SSL"] = auth_require_ssl
+      ENV["SSO_COOKIE_DOMAIN"] = sso_cookie_domain
     end
 
     specify { Exvo::Helpers.cdn_host.should eql(cdn_host) }
-    specify { Exvo::Helpers.sso_cookie_domain.should eql(sso_cookie_domain) }
     specify { Exvo::Helpers.auth_client_id.should eq(auth_client_id) }
     specify { Exvo::Helpers.auth_client_secret.should eq(auth_client_secret) }
     specify { Exvo::Helpers.auth_uri.should match(/http:\/\//) }
+    specify { Exvo::Helpers.sso_cookie_domain.should eql(sso_cookie_domain) }
 
     after do
       ENV["CDN_HOST"] = nil
-      ENV["SSO_COOKIE_DOMAIN"] = nil
       ENV["AUTH_CLIENT_ID"] = nil
       ENV["AUTH_CLIENT_SECRET"] = nil
       ENV["AUTH_REQUIRE_SSL"] = nil
+      ENV["SSO_COOKIE_DOMAIN"] = nil
     end
   end
 
   describe "direct settings override the defaults" do
     # all *host* methods are defined the same using metaprogramming, only testing for 1 is enough
     let(:cdn_host) { "new.cdn.exvo.com" }
-    let(:sso_cookie_domain) { "exvo.new" }
+    let(:auth_client_id) { '123' }
+    let(:auth_client_secret) { 'abc' }
     let(:auth_require_ssl) { false }
+    let(:sso_cookie_domain) { "exvo.new" }
 
     before do
       Exvo::Helpers.cdn_host = cdn_host
-      Exvo::Helpers.sso_cookie_domain = sso_cookie_domain
+      Exvo::Helpers.auth_client_id = auth_client_id
+      Exvo::Helpers.auth_client_secret = auth_client_secret
       Exvo::Helpers.auth_require_ssl = auth_require_ssl
+      Exvo::Helpers.sso_cookie_domain = sso_cookie_domain
     end
 
     specify { Exvo::Helpers.cdn_host.should eql(cdn_host) }
-    specify { Exvo::Helpers.sso_cookie_domain.should eql(sso_cookie_domain) }
+    specify { Exvo::Helpers.auth_client_id.should eq(auth_client_id) }
+    specify { Exvo::Helpers.auth_client_secret.should eq(auth_client_secret) }
     specify { Exvo::Helpers.auth_uri.should match(/http:\/\//) }
+    specify { Exvo::Helpers.sso_cookie_domain.should eql(sso_cookie_domain) }
 
     after do
       Exvo::Helpers.cdn_host = nil
-      Exvo::Helpers.sso_cookie_domain = nil
+      Exvo::Helpers.auth_client_id = nil
+      Exvo::Helpers.auth_client_secret = nil
       Exvo::Helpers.auth_require_ssl = nil
+      Exvo::Helpers.sso_cookie_domain = nil
     end
   end
 
